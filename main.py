83b2821bd708a1bec001b455cb0d45748bd9cf9f
morgoth95 <diego.fiori@epfl.ch>
2022-03-21 00:43:51 +0100

    add static behaviour

--- main.py
+++ main.py
@@ -55,6 +55,32 @@ def optimized_and_run(text, model, tokenizer, save_dir):
         json.dump(time_dict, f_out)
 
 
+def optimized_and_run_static(text, model, tokenizer, save_dir):
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    encoded_input = tokenizer(text, return_tensors='pt', truncation=True)
+    extra_input_info = [{}] + [{"max_value": 1, "min_value": 0}] * (len(encoded_input) - 1)
+    vanilla_time = run_hugginface_model(model.to(device), encoded_input.to(device))
+    with TemporaryDirectory() as tmp_dir:
+        optimized_model = optimize_huggingface_model(
+            model=model,
+            tokenizer=tokenizer,
+            target_text=text,
+            batch_size=1,
+            max_input_sizes=[tuple(value.size()[1:]) for value in
+                             encoded_input.values()],
+            save_dir=tmp_dir,
+            extra_input_info=extra_input_info,
+            use_torch_api=False,
+            use_static_shape=True,
+        )
+        optimized_time = run_hugginface_model(optimized_model, encoded_input)
+    time_dict = {
+        "vanilla_time": vanilla_time, "optimized_time": optimized_time
+    }
+    with open(os.path.join(save_dir, "time_info.json"), "w") as f_out:
+        json.dump(time_dict, f_out)
+
+
 if __name__ == "__main__":
     from transformers import GPT2Tokenizer, GPT2Model
 
@@ -62,6 +88,9 @@ if __name__ == "__main__":
     model = GPT2Model.from_pretrained('gpt2')
     text = "Replace me by any text you'd like."
     optimized_and_run(text, model, tokenizer, save_dir="gpt2")
+    optimized_and_run_static(text, model, tokenizer, "gpt2-static-short")
+    long_text = " ".join([text]*200)
+    optimized_and_run_static(long_text, model, tokenizer, "gpt2-static-long")
 
     from transformers import BertTokenizer, BertModel
 
@@ -69,4 +98,6 @@ if __name__ == "__main__":
     model = BertModel.from_pretrained("bert-base-uncased")
     text = "Hello, my dog is cute"
     optimized_and_run(text, model, tokenizer, save_dir="bert")
-
+    optimized_and_run_static(text, model, tokenizer, "bert-static-short")
+    long_text = " ".join([text] * 200)
+    optimized_and_run_static(long_text, model, tokenizer, "bert-static-long")
