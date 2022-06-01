9a43877928dee872c26b835d95638895975c0a44
morgoth95 <diego.fiori@epfl.ch>
2022-06-01 17:31:48 +0200

    edit batch-size

--- nebulgym/run_custom_model.py
+++ nebulgym/run_custom_model.py
@@ -12,6 +12,8 @@ import torch
 
 from nebulgym.annotations.torch_annotations import patch_torch_module, patch_dataset
 
+BACKEND = "PYTORCH"
+
 
 def get_fastai_dataloaders():
     path = untar_data(URLs.PETS)
@@ -76,9 +78,9 @@ def compute_accuracy(pred, target):
         return torch.mean(1.*(pred == target))
 
 
-def initialize_model_and_data(dls):
+def initialize_model_and_data(dls, bs):
     ds = CustomDataset(dls.train)
-    dl = DataLoader(ds, batch_size=8)
+    dl = DataLoader(ds, batch_size=bs)
     model = CustomModel()
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
     cost_fun = torch.nn.CrossEntropyLoss()
@@ -108,9 +110,9 @@ def train_model(dl, model, optimizer, cost_fun, max_epochs):
     return total_time
 
 
-def train_and_save(max_epochs, json_dict):
+def train_and_save(max_epochs, bs, json_dict):
     dls = get_fastai_dataloaders()
-    dl, model, optimizer, cost_fun = initialize_model_and_data(dls)
+    dl, model, optimizer, cost_fun = initialize_model_and_data(dls, bs)
     total_time = train_model(dl, model, optimizer, cost_fun, max_epochs)
     name = f"standard_{max_epochs}"
     json_dict[name] = total_time
@@ -135,7 +137,7 @@ class CustomPatchedDataset(Dataset):
         return len(self._dl)
 
 
-@patch_torch_module(patch_backprop=True, backends=["PYTORCH"])
+@patch_torch_module(patch_backprop=True, backends=[BACKEND])
 class CustomPatchedModel(Module):
     def __init__(self):
         super().__init__()
@@ -164,18 +166,18 @@ class CustomPatchedModel(Module):
         return self._linears(x)
 
 
-def initialize_fast_model_and_data(dls):
+def initialize_fast_model_and_data(dls, bs):
     ds = CustomPatchedDataset(dls.train)
-    dl = DataLoader(ds, batch_size=8)
+    dl = DataLoader(ds, batch_size=bs)
     model = CustomPatchedModel()
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
     cost_fun = torch.nn.CrossEntropyLoss()
     return dl, model, optimizer, cost_fun
 
 
-def train_and_save_with_nebulgym(max_epochs, json_dict):
+def train_and_save_with_nebulgym(max_epochs, bs, json_dict):
     dls = get_fastai_dataloaders()
-    dl, model, optimizer, cost_fun = initialize_fast_model_and_data(dls)
+    dl, model, optimizer, cost_fun = initialize_fast_model_and_data(dls, bs)
     total_time = train_model(dl, model, optimizer, cost_fun, max_epochs)
     name = f"nebulgym_{max_epochs}"
     json_dict[name] = total_time
@@ -186,10 +188,12 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--epochs", "-e", type=int)
+    parser.add_argument("--batch_size", "-bs", type=int)
     args = parser.parse_args()
+    bs = args.batch_size or 8
     max_epochs = args.epochs or 10
     json_dict = {}
-    train_and_save(max_epochs, json_dict)
-    train_and_save_with_nebulgym(max_epochs, json_dict)
+    train_and_save(max_epochs, bs, json_dict)
+    train_and_save_with_nebulgym(max_epochs, bs, json_dict)
     with open("results.json", "w") as f:
         json.dump(json_dict, f)
