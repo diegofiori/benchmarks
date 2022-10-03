63a51277cb1121f9f50edc4aa4dc545f059aed28
morgoth95 <d.fiori@nebuly.ai>
2022-10-03 16:12:21 +0200

    fix bug

--- experiments/hrnet/cutlass_extensions.py
+++ experiments/hrnet/cutlass_extensions.py
@@ -319,7 +319,7 @@ class CutlassConv2d(torch.nn.Module):
             self.split_k_slices,
             self.conv_kind,
             self.reduction_operation,
-        ).permute(0, 3, 1, 2)  #  .contiguous().view(N, self.out_H, self.out_W, self.out_C).permute(0, 3, 1, 2)
+        ).contiguous().view(N, self.out_H, self.out_W, self.out_C).permute(0, 3, 1, 2)
 
     @classmethod
     def from_conv2d(cls, input_shape: Tuple[int, int, int], conv: torch.nn.Conv2d):
