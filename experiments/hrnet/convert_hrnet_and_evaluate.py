7d5179f1ecc3315ba240d3329680c1a5ae997ab1
morgoth95 <d.fiori@nebuly.ai>
2022-11-17 11:05:26 +0100

    add onnx conversion

--- experiments/hrnet/convert_hrnet_and_evaluate.py
+++ experiments/hrnet/convert_hrnet_and_evaluate.py
@@ -39,7 +39,7 @@ def compute_latency_onnx(model_path: str, input_data: List[np.ndarray]):
     latencies = []
     transform = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     for input_tensor in input_data:
-        input_tensor = transform(torch.from_numpy(input_tensor) / 255).numpy()
+        input_tensor = transform(torch.from_numpy(input_tensor) / 255).unsqueeze(0).numpy()
         st = time.time()
         sess.run([output_name], {input_name: input_tensor})
         latencies.append(time.time() - st)
