fafd7c8487592ca2b640b7f5ab53165e057e9206
diegofiori <d.fiori@nebuly.ai>
2023-01-16 22:02:30 +0100

    add test for tf model

---
+++ test_speedster/test_tensorflow.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+import numpy as np
+from speedster import optimize_model
+
+
+def test_fully_connected_tensorflow_model():
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(224, 224, 3)),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(1024, activation='relu'),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(1024, activation='relu'),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(1024, activation='relu'),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(1024, activation='relu'),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(1024, activation='relu'),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(10, activation='softmax')
+    ])
+    model.compile(optimizer='adam',
+                    loss='sparse_categorical_crossentropy',
+                    metrics=['accuracy'])
+    input_data = [
+        ((tf.random.normal([1, 224, 224, 3]),), 0) for i in range(100)
+    ]
+    optimized_model = optimize_model(model, input_data, metric_drop_ths=0.1)
+
+
+if __name__ == "__main__":
+    test_fully_connected_tensorflow_model()
