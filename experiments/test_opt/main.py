ceafa7aaae4877e0b95b2bc55aa0f40e96b8d1a4
diegofiori <d.fiori@nebuly.ai>
2023-03-28 21:34:07 +0000

    Add half

--- experiments/test_opt/main.py
+++ experiments/test_opt/main.py
@@ -4,10 +4,10 @@ import time
 import numpy as np
 
 tokenizer = AutoTokenizer.from_pretrained("facebook/opt-1.3b")
-model = AutoModelForCausalLM.from_pretrained("facebook/opt-1.3b").to("cuda")
+model = AutoModelForCausalLM.from_pretrained("facebook/opt-1.3b").to("cuda").half()
 
 
-def compute_latency(hf_model, input_dict, n_warmup=5, n_repeat=10):
+def compute_latency(hf_model, input_dict, max_length,  n_warmup=5, n_repeat=5):
     model.eval()
 
     for _ in range(n_warmup):
@@ -15,21 +15,23 @@ def compute_latency(hf_model, input_dict, n_warmup=5, n_repeat=10):
             hf_model(**input_dict)
 
     latencies = []
-    for _ in range(n_repeat):
+    for i in range(n_repeat):
+        print(i)
         with torch.no_grad():
             start = time.time()
-            hf_model.generate(**input_dict, max_length=1024, num_beams=5)
+            hf_model.generate(**input_dict, max_length=max_length, num_beams=5)
             end = time.time()
         latencies.append(end - start)
     return np.mean(latencies)
 
 
 input_text = "\n".join(["Hello, my dog is cute."] * 300)
-input_hf = tokenizer(input_text, return_tensors="pt", truncation=True).to("cuda")
-long_latency = compute_latency(model, input_hf)
+input_hf = tokenizer(input_text, return_tensors="pt", truncation=True, max_length=1024).to("cuda")
+print(input_hf["input_ids"].shape)
+long_latency = compute_latency(model, input_hf, 2048)
 print(f"tokens: {input_hf['input_ids'].shape[1]}, latency: {long_latency}")
 input_text = "\n".join(["Hello, my dog is cute."] * 150)
-input_hf = tokenizer(input_text, return_tensors="pt", truncation=True).to("cuda")
-short_latency = compute_latency(model, input_hf)
+input_hf = tokenizer(input_text, return_tensors="pt", truncation=True, max_length=512).to("cuda")
+short_latency = compute_latency(model, input_hf, 1536)
 print(f"tokens: {input_hf['input_ids'].shape[1]}, latency: {short_latency}")
 print(f"speedup: {long_latency / short_latency}")
